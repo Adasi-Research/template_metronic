01917f9ea386eaf85246c6a19d8be81b0e3e5bf5
Jônatas Alves <49556955+JonatasAlves9@users.noreply.github.com>
2023-01-31 15:34:16 -0300

    feat: adjustments in domain and data

---
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -0,0 +1,27 @@
+import {RemoteAuthentication} from "./remote-authentication";
+import {HttpPostClientSpy} from "../../test/mock-http-client";
+
+type SutTypes = {
+    sut: RemoteAuthentication;
+    httpPostClientSpy: HttpPostClientSpy;
+};
+
+// Fabric
+const makeSut = (url: string = ""): SutTypes => {
+    const httpPostClientSpy = new HttpPostClientSpy();
+    const sut = new RemoteAuthentication(url, httpPostClientSpy);
+
+    return {
+        sut,
+        httpPostClientSpy,
+    };
+};
+
+describe("RemoteAuthentication", () => {
+    test("Should call HttpPostClient with correct url", async () => {
+        const url = "other_url";
+        const {sut, httpPostClientSpy} = makeSut(url);
+        await sut.auth();
+        expect(httpPostClientSpy.url).toBe(url);
+    });
+});
---
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -0,0 +1,13 @@
+import {HttpPostClient} from "@/src/data/protocols/http/http-post-client";
+
+export class RemoteAuthentication {
+    constructor(
+        private readonly url: string,
+        private readonly httpClient: HttpPostClient
+    ) {
+    }
+
+    async auth(): Promise<void> {
+        await this.httpClient.post(this.url);
+    }
+}
