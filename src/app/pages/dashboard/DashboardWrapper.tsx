7c8ea59dbaf0093ccaef6fa43873a972e54bf250
Jônatas Alves <49556955+JonatasAlves9@users.noreply.github.com>
2023-01-31 14:30:05 -0300

    some adjustments

--- src/app/pages/dashboard/DashboardWrapper.tsx
+++ src/app/pages/dashboard/DashboardWrapper.tsx
@@ -3,98 +3,92 @@ import {FC} from 'react';
 import {useIntl} from 'react-intl';
 import {PageTitle, useLayout} from '../../../config/layout/core';
 import {useAppDispatch, useAppSelector} from '../../../config/hooks/useRedux';
-import {Button} from 'react-bootstrap';
-import {
-  decrement,
-  increment,
-  incrementByAmount,
-} from '../../../config/store/dashboard/counterSlice';
 
 import DataTable, {TableColumn} from 'react-data-table-component';
 
 interface DataRow {
-  title: string;
-  director: string;
-  year: string;
+    title: string;
+    director: string;
+    year: string;
 }
 
 const DashboardWrapper: FC = () => {
-  const intl = useIntl();
+    const intl = useIntl();
 
-  const {config} = useLayout();
+    const {config} = useLayout();
 
-  console.log(config);
+    console.log(config);
 
-  const count = useAppSelector((state) => state.counter.value);
-  const dispatch = useAppDispatch();
+    const count = useAppSelector((state) => state.counter.value);
+    const dispatch = useAppDispatch();
 
-  const daterangepickerButtonClass = config.app?.toolbar?.fixed?.desktop
-    ? 'btn-danger'
-    : 'bg-body btn-color-gray-700 btn-active-color-primary';
+    const daterangepickerButtonClass = config.app?.toolbar?.fixed?.desktop
+        ? 'btn-danger'
+        : 'bg-body btn-color-gray-700 btn-active-color-primary';
 
-  const data: DataRow[] = [
-    {
-      title: 'Oi',
-      director: 'Amigo',
-      year: '2022',
-    },
-    {
-      title: 'Oi',
-      director: 'Amigo',
-      year: '2022',
-    },
-    {
-      title: 'Oi',
-      director: 'Amigo',
-      year: '2022',
-    },
-    {
-      title: 'Oi',
-      director: 'Amigo',
-      year: '2022',
-    },
-    {
-      title: 'Oi',
-      director: 'Amigo',
-      year: '2022',
-    },
-    {
-      title: 'Oi',
-      director: 'Amigo',
-      year: '2022',
-    },
-    {
-      title: 'Oi z',
-      director: 'Amigo',
-      year: '2022',
-    },
-  ];
+    const data: DataRow[] = [
+        {
+            title: 'Oi',
+            director: 'Amigo',
+            year: '2022',
+        },
+        {
+            title: 'Oi',
+            director: 'Amigo',
+            year: '2022',
+        },
+        {
+            title: 'Oi',
+            director: 'Amigo',
+            year: '2022',
+        },
+        {
+            title: 'Oi',
+            director: 'Amigo',
+            year: '2022',
+        },
+        {
+            title: 'Oi',
+            director: 'Amigo',
+            year: '2022',
+        },
+        {
+            title: 'Oi',
+            director: 'Amigo',
+            year: '2022',
+        },
+        {
+            title: 'Oi z',
+            director: 'Amigo',
+            year: '2022',
+        },
+    ];
 
-  const columns: TableColumn<DataRow>[] = [
-    {
-      name: 'Title',
-      selector: (row) => row.title,
-      sortable: true,
-    },
-    {
-      name: 'Director',
-      selector: (row) => row.director,
-    },
-    {
-      name: 'Year',
-      selector: (row) => row.year,
-    },
-  ];
+    const columns: TableColumn<DataRow>[] = [
+        {
+            name: 'Title',
+            selector: (row) => row.title,
+            sortable: true,
+        },
+        {
+            name: 'Director',
+            selector: (row) => row.director,
+        },
+        {
+            name: 'Year',
+            selector: (row) => row.year,
+        },
+    ];
 
-  return (
-    <>
-      <PageTitle breadcrumbs={[]}>{intl.formatMessage({id: 'MENU.DASHBOARD'})}</PageTitle>
-      <Button onClick={() => dispatch(incrementByAmount(25))}>{count}</Button>
-      <Button onClick={() => dispatch(increment())}>{count}</Button>
-      <Button onClick={() => dispatch(decrement())}>{count}</Button>
-      <DataTable columns={columns} data={data} />
-    </>
-  );
+    return (
+        <>
+            <PageTitle breadcrumbs={[]}>{intl.formatMessage({id: 'MENU.DASHBOARD'})}</PageTitle>
+            {/*<Button onClick={() => dispatch(incrementByAmount(25))}>{count}</Button>*/}
+            {/*<Button onClick={() => dispatch(increment())}>{count}</Button>*/}
+            {/*<Button onClick={() => dispatch(decrement())}>{count}</Button>*/}
+            <DataTable columns={columns} data={data} pagination/>
+        </>
+    );
 };
 
 export {DashboardWrapper};
