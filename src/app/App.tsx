a4936625269e612b9d4e5d8dc002f5d4417aa2eb
jonatasAlves9 <alvesjonatas99@gmail.com>
2023-01-31 22:17:08 -0300

    chore: adjustment in eslint

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,10 @@
-import {Suspense} from 'react'
-import {Outlet} from 'react-router-dom'
-import {I18nProvider} from '../config/i18n/i18nProvider'
-import {LayoutProvider, LayoutSplashScreen} from '../config/layout/core'
-import {MasterInit} from '../config/layout/MasterInit'
-import {AuthInit} from './modules/auth'
+import React, { Suspense } from 'react'
+
+import { Outlet } from 'react-router-dom'
+import { I18nProvider } from '../config/i18n/i18nProvider'
+import { LayoutProvider, LayoutSplashScreen } from '../config/layout/core'
+import { MasterInit } from '../config/layout/MasterInit'
+import { AuthInit } from './modules/auth'
 
 const App = () => {
   return (
@@ -20,4 +21,4 @@ const App = () => {
   )
 }
 
-export {App}
+export { App }
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
-import {createRoot} from 'react-dom/client';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 // Axios
 import axios from 'axios';
-import {Chart, registerables} from 'chart.js';
-import {QueryClient, QueryClientProvider} from 'react-query';
-import {ReactQueryDevtools} from 'react-query/devtools';
+import { Chart, registerables } from 'chart.js';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 // Apps
-import {MetronicI18nProvider} from './config/i18n/Metronici18n';
+import { MetronicI18nProvider } from './config/i18n/Metronici18n';
 /**
  * TIP: Replace this style import with rtl styles to enable rtl mode
  *
@@ -14,10 +15,10 @@ import {MetronicI18nProvider} from './config/i18n/Metronici18n';
 import './config/assets/sass/style.scss';
 import './config/assets/sass/plugins.scss';
 import './config/assets/sass/style.react.scss';
-import {AppRoutes} from './app/routing/AppRoutes';
-import {AuthProvider, setupAxios} from './app/modules/auth';
-import {Provider} from 'react-redux';
-import {store} from './config/store';
+import { AppRoutes } from './app/routing/AppRoutes';
+import { AuthProvider, setupAxios } from './app/modules/auth';
+import { Provider } from 'react-redux';
+import { store } from './config/store';
 /**
  * Creates `axios-mock-adapter` instance for provided `axios` instance, add
  * basic Metronic mocks and returns it.
@@ -29,11 +30,11 @@ import {store} from './config/store';
  *
  * @see https://github.com/axios/axios#interceptors
  */
-setupAxios(axios);
-Chart.register(...registerables);
+setupAxios(axios)
+Chart.register(...registerables)
 
-const queryClient = new QueryClient();
-const container = document.getElementById('root');
+const queryClient = new QueryClient()
+const container = document.getElementById('root')
 if (container) {
   createRoot(container).render(
     <QueryClientProvider client={queryClient}>
@@ -46,5 +47,5 @@ if (container) {
         <ReactQueryDevtools initialIsOpen={false} />
       </Provider>
     </QueryClientProvider>
-  );
+  )
 }
